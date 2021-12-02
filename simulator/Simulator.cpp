b8a44e82896817149f2c04a48515eb623d06c051
Jack Wiggall <72393388+jackwiggall@users.noreply.github.com>
2021-12-02 09:58:38 +0000

    basic display

--- simulator/Simulator.cpp
+++ simulator/Simulator.cpp
@@ -1,8 +1,10 @@
 #include "Simulator.h"
+#include "Colors.h"
 
 int main() {
 
     Simulator sim;
+    sim.display();
     return 0;
 }
 
@@ -46,4 +48,41 @@ void Simulator::display() {
     //memory 32xnum grid
     //2 columns
     //coloured squares?
+
+    //display content shows memory, CI, PI, accum, STOP
+    //Display is printed after each cycle
+    //text, text-based graphics, colour?
+
+    //each step in the fetch-dec-exe cycle the state of each part
+    //of the BABY hardware, i.e displaying register values, memory values and I/O
+    
+    /**
+     * int num = 3;
+     * string output;
+     * output += to_string(num);
+     * cout << output << endl;**/
+
+    int value = 0; //default value to be changed later once data is given
+
+    //outputs 0 if the strings pi and acc are empty
+    string piVal = pi;
+    if (pi=="") {
+        piVal = "0";
+    }
+    string accVal = acc;
+    if (acc=="") {
+        accVal = "0";
+    }
+
+
+    cout << "\n" << BOLD(FGRN("Memory")) << "\t\t" << value << endl; //store?
+    cout << BOLD(FGRN("MemoryVal")) << "\t" << &store << endl; //pointer of store
+    cout << BOLD(FGRN("RegisterVal")) << "\t" << &value << endl; //pointer of register
+    cout << BOLD(FGRN("CI")) << "\t\t" << ci << endl;
+    cout << BOLD(FGRN("PI")) << "\t\t" << piVal << endl;
+    cout << BOLD(FGRN("Accumulator")) << "\t" << accVal << endl; //idk
+    cout << BOLD(FGRN("I/O")) << "\t\t" << value << endl;
+    cout << BOLD(FGRN("STOP")) << endl;
+    
+    //🔲 ⬜ ⬛ emoji idk
 }
