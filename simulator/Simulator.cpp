e56ade030599d55d1e671334971cab961bde3072
HeatherCurrie <heathercurrie1000@gmail.com>
2021-12-04 13:45:58 +0000

    setup function

--- simulator/Simulator.cpp
+++ simulator/Simulator.cpp
@@ -3,6 +3,7 @@
 int main() {
 
     Simulator sim;
+    sim.setup();
     return 0;
 }
 
@@ -10,10 +11,41 @@ Simulator::Simulator(){
     //
 }
 
+/**
+ *  Function sets up the simulator by setting the starting values (memory size and info)
+ */
 bool Simulator::setup(){
-    //get memorySize
-    //call load
-    return false;
+    char infoChoice;
+    int memoryChoice;
+
+    //get user input for if info should be displayed
+    cout << "\nWould you like to display fetch execute explanation while running? [y/n]" << endl;
+    cin >> infoChoice;
+
+    //info validation
+    if (infoChoice == 'y') {
+        info = true;
+    } else if (infoChoice == 'n') {
+        info = false;
+    } else {
+        cout << "invalid input, set to true as default" << endl;
+    }
+
+    //get memory size user input
+    cout << "What would you like the memory size to be, standard is 32 (must be between 1 and 64)?" << endl;
+    cin >> memoryChoice;
+
+    //validation for the memory size 
+    if ((memoryChoice > 0) && (memoryChoice <= 64)) {
+        memsize = memoryChoice;
+    } else {
+        cout << "invalid input, set to 32 as default" << endl;
+    }
+
+    //call load and check if successful
+    if (loadProgram() == true);
+        ready = true;
+    return true;
 }
 
 bool Simulator::loadProgram(){
