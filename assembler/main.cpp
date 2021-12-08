0cf64828101170f30576dc670fb9e33b0da7e30b
Kai Uerlichs <2421101@dundee.ac.uk>
2021-12-08 21:10:10 +0000

    some stuff

--- assembler/main.cpp
+++ assembler/main.cpp
@@ -1,10 +1,34 @@
 #include "Assembler.h"
 #include <vector>
+using namespace std;
 
-// ./assemble source destination isntructionset memory 
+/**
+ * @brief Displays "Manchester Baby Simulator" using ASCII art
+ */
+void displayBigFont(){
+    cout << endl;
+    cout << "\x1B[34m█▀ \x1B[36m█▀ \x1B[32m█▀▀ \x1B[33m█▀▄▀█   \x1B[35m▄▀█ \x1B[31m█▀ \x1B[34m█▀ \x1B[36m█▀▀ \x1B[32m█▀▄▀█ \x1B[33m█▄▄ \x1B[35m█   \x1B[31m█▀▀ \x1B[34m█▀█" << endl;
+    cout << "\x1B[34m▄█ \x1B[36m▄█ \x1B[32m██▄ \x1B[33m█ ▀ █   \x1B[35m█▀█ \x1B[31m▄█ \x1B[34m▄█ \x1B[36m██▄ \x1B[32m█ ▀ █ \x1B[33m█▄█ \x1B[35m█▄▄ \x1B[31m██▄ \x1B[34m█▀▄" << endl;
+    cout << endl;
+}
+
+/**
+ * @brief Get input from user
+ * 
+ * @param prompt Message to display
+ * @return string The user input
+ */
+string getUserInput(string prompt){
+    cout << FMAG("[Assembler] ") << prompt << endl;
+    return "";
+}
+
+void startAssembly(){
+    string ifile = getUserInput("Please enter the path to your ")
+}
+
 int main() { 
-    Assembler a(32);
-    a.loadInstructionSet("./SSEM-Instructions.txt");
-    a.assemble("./programs/test.txt");
-    a.exportToFile("assemlbed.txt");
+    displayBigFont();
+    startAssembly();
+    return 1;
 }
